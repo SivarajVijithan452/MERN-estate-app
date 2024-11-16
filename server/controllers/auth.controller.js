580be5120bc2412478482d2baf716f5ec742a96f
Sivaraj Vijithan <vijithan678@gmail.com>
2024-11-16 22:49:47 +0530

    create sign up api route

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,37 @@
+import User from './../models/user.model.js';
+import bcrypt from 'bcryptjs';
+
+export const signup = async (req, res) => { 
+    try {
+        const { username, email, password } = req.body;
+        
+        // Check if username or email already exists
+        const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+        if (existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: existingUser.username === username 
+                    ? 'Username already exists' 
+                    : 'Email already exists'
+            });
+        }
+
+        const hashedPassword = await bcrypt.hashSync(password, 10);
+        const newUser = new User({ username, email, password: hashedPassword });
+        await newUser.save();
+        res.status(201).json({
+            success: true,
+            message: 'User created successfully',
+            data: newUser
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: error.name === 'ValidationError' 
+                ? 'Invalid input data' 
+                : 'Internal server error',
+            error: error.message
+        });
+    }
+ }
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './routes/user.route.js';
+import authRoute from './routes/auth.route.js';
 dotenv.config();
 
 // Connect to MongoDB
@@ -12,9 +13,11 @@ mongoose.connect(process.env.MONGO).then(() => {
 });
 
 const app = express();
+app.use(express.json());
 
 // Routes
 app.use('/api/user', userRoute);
+app.use('/api/auth', authRoute);
 
 // Server
 app.listen(3000, () => {

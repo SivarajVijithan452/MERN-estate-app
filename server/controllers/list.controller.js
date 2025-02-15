b9c4afbf60b00f5d2ba356eadb31b498794f2264
Sivarj vijithan <vijithan678@gmail.com>
2025-02-15 08:37:32 +0530

    create search api

--- server/controllers/list.controller.js
+++ server/controllers/list.controller.js
@@ -103,3 +103,50 @@ export const getListing = async (req, res) => {
         });
     }
 }
+
+export const getListings = async (req, res) => {
+    try {
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        let offer = req.query.offer;
+        if (offer === 'undefined' || offer === 'false') {
+            offer = { $in : [false, true] };
+        }
+        let furnished = req.query.furnished;
+        if (furnished === 'undefined' || furnished === 'false') {
+            furnished = { $in : [false, true] };
+        }
+        let parking = req.query.parking;
+        if (parking === 'undefined' || parking === 'false') {
+            parking = { $in : [false, true] };
+        }
+        let type = req.query.type;
+        if (type === 'undefined' || type === 'all') {
+            type = { $in : ['sale', 'rent'] };
+        }
+        const searchTerm = req.query.searchTerm;
+        const sort = req.query.sort || 'createdAt';
+        const order = req.query.order || 'desc';
+        const listings = await Listing.find({
+            offer,
+            furnished,
+            parking,
+            type,
+            name: {
+                $regex: searchTerm,
+                $options: 'i'
+            }
+        }).sort({ [sort]: order }).limit(limit).skip(startIndex);
+        return res.status(200).json({
+            success: true,
+            listings
+        });
+    } catch (error) {
+        console.log("error", error);
+        res.status(500).json({
+            success: false,
+            message: "Error getting listings",
+            error: error.message
+        });
+    }
+}
